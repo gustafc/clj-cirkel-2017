d7092df142e46877f7e097f4e4fe4a972209818f
Gustaf Cele <gustafc@gmail.com>
2017-03-05 15:06:44 +0100

    ch 1

--- src/clj_cirkel_2017/ch01/precedence.clj
+++ src/clj_cirkel_2017/ch01/precedence.clj
@@ -1,3 +1,11 @@
 ; Talking about precedence in 1.2.2 - is Clj ltr or rtl?
 [(/ 100 2 4)
- (/ (/ 100 2) 4)]
+ (/ (/ 100 2) 4)
+ (/ 100 (/ 2 4))]
+
+(defn print-and-return [n]
+  (println "*" n)
+  n)
+
+; Seems like ltr!
+(+ (print-and-return 1) (print-and-return 2))
