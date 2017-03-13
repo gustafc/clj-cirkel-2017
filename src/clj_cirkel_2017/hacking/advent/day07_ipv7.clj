fddf5bd20463ead3f19e6e89c84ba0681c92dafd
Gustaf Cele <gustafc@gmail.com>
2017-03-13 20:41:33 +0100

    Day 07, part 2

--- src/clj_cirkel_2017/hacking/advent/day07_ipv7.clj
+++ src/clj_cirkel_2017/hacking/advent/day07_ipv7.clj
@@ -15,7 +15,31 @@
 ;     ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even though it's within a larger string).
 ;
 ; How many IPs in your puzzle input support TLS?
-
+;
+; --- Part Two ---
+;
+; You would also like to know which IPs support SSL (super-secret listening).
+;
+; An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in the supernet sequences (outside any
+; square bracketed sections), and a corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences. An
+; ABA is any three-character sequence which consists of the same character twice with a different character between
+; them, such as xyx or aba. A corresponding BAB is the same characters but in reversed positions: yxy and bab,
+; respectively.
+;
+; For example:
+;
+;     aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab within square brackets).
+;
+;     xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+;
+;     aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related,
+;     because the interior character must be different).
+;
+;     zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz
+;     overlap).
+;
+; How many IPs in your puzzle input support SSL?
+;
 
 (ns advent-07
   (:use clojure.test)
@@ -69,6 +93,40 @@
     (some is-abba? supernet)
     ))
 
+(defn extract-aba
+  {:test #(do
+            (is (= [] (extract-aba "ba")))
+            (is (= [] (extract-aba "asdfghjk")))
+            (is (= ["aba"] (extract-aba "aba")))
+            (is (= ["aba" "kek"] (extract-aba "njkabakkkek")))
+            )}
+  [s]
+  (if (< (count s) 3)
+    []
+    (->> (map vector s (drop 1 s) (drop 2 s))
+         (filter (fn [[a b c]] (and (not= a b) (= a c))))
+         (map strings/join)
+         )))
+
+(defn aba-to-bab
+  {:test #(is (= "bab" (aba-to-bab "aba")))}
+  [s]
+  (str (subs s 1 3) (subs s 1 2))
+  )
+
+(defn supports-ssl?
+  {:test #(do
+            (is (supports-ssl? (parse-ipv7 "aba[bab]xyz")))
+            (is (not (supports-ssl? (parse-ipv7 "xyx[xyx]xyx"))))
+            (is (supports-ssl? (parse-ipv7 "aaa[kek]eke")))
+            (is (supports-ssl? (parse-ipv7 "zazbz[bzb]cdb")))
+            )}
+  [{:keys [supernet hypernet] :as inp}]
+  (let [super-aba (into #{} (mapcat extract-aba supernet))
+        hyper-bab (mapcat #(map aba-to-bab (extract-aba %)) hypernet)]
+    (some #(contains? super-aba %) hyper-bab)
+    ))
+
 (def input (->> (clojure.java.io/file "./resources/advent/day07_ip_addrs.txt")
                 (slurp)
                 (strings/split-lines)
@@ -83,6 +141,14 @@
        (count)
        ))
 
+(defn solve-part-2
+  {:test #(is (= 231 (solve-part-2)))}
+  []
+  (->> input
+       (filter supports-ssl?)
+       (count)
+       ))
+
 
 (prn "Part 1: " (solve-part-1))
-;(prn "Part 2: " (solve-part-2))
+(prn "Part 2: " (solve-part-2))
