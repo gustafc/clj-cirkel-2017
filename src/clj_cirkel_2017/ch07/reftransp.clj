6ea84dddd1a4bf32cb809ebfb5ea6d3ddca270dd
Gustaf Cele <gustafc@gmail.com>
2017-04-03 09:13:58 +0200

    Experimenting with def

---
+++ src/clj_cirkel_2017/ch07/reftransp.clj
@@ -0,0 +1,11 @@
+(def data "apelsin")
+
+(defn get-data [] data)
+
+(println "Data 1:" data)
+(println "Get data 1:" (get-data))
+
+(def data "banan")
+
+(println "Data 2:" data)
+(println "Get data 2:" (get-data))
